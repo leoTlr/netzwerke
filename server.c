9ea393434ad61c0783a19f906807ca100dd0e4ce
leoTlr <leo.teuchtler@hotmail.de>
2018-10-18 15:52:08 +0200

    change pthread_t started_threads array to vector datatype

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <pthread.h>
 
 #include "helper_funcs.c"
+#include "vector.c"
 
 #define MAX_CONNECTIONS 10
 #define MAX_THREADS MAX_CONNECTIONS
@@ -22,28 +23,14 @@ typedef struct {
 
 // thread body and exit handler
 static void connection_thread(void *);
-static void thread_exithdlr(void *);
+static void thread_exithandler(void *);
 
-pthread_t started_threads[MAX_THREADS]; // set data type qould be nice here
+static void sighandler(int);
+
+// global vars needed for signalhandler
+CVector* started_threads;
 int server_sockfd = -1, client_sockfd;
-
-// ensure closing socket when recieving SIGINT
-static void sighandler(int signo){
-	// exiting normally (with exit(0)) should invoke the threads exithandlers
-	// EDIT: seems not to; TODO
-	switch (signo){
-		case SIGINT:
-			printf("\nSIGINT recieved. Closing sockets\n");
-			try_close(server_sockfd);
-			exit(EXIT_SUCCESS);
-		case SIGTERM:
-			printf("\nSIGTERM recieved. Closing server socket: ");
-			try_close(server_sockfd);
-			exit(EXIT_FAILURE);
-		default:
-			break;
-	}
-}
+volatile sig_atomic_t exit_requested = 0;
 
 // Called with wrong arguments.
 void usage(char *argv0){
@@ -53,7 +40,18 @@ void usage(char *argv0){
 
 int main(int argc, char **argv){
 
-	int started_threads_counter = 0;
+	// prepare thread vector
+	started_threads = malloc(sizeof(CVector));
+	CVector_init(started_threads);
+	if (started_threads == NULL){
+		sys_err("CVector_init", 2, server_sockfd);
+	}
+	pthread_t tidBUF;
+
+	// other declarations
+	int client_id_counter = 1;
+
+	// declarations for sockets
 	struct sockaddr_in server_addr, client_addr; // getting casted to sockaddr on usage
 	socklen_t addrlen = sizeof(struct sockaddr_in);
 
@@ -69,6 +67,7 @@ int main(int argc, char **argv){
 	// Check for right number of arguments
 	if (argc < 2) usage(argv[0]);
 
+	// initialize TCP/IP socket
 	if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		sys_err("Server Fault : SOCKET", -1, server_sockfd);
 	}
@@ -76,7 +75,7 @@ int main(int argc, char **argv){
 	// Set params so that we receive IPv4 packets from anyone on the specified port
 	memset(&server_addr, 0, addrlen);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY = 0 (ipv4 0.0.0.0)
-	server_addr.sin_family      = AF_INET;
+	server_addr.sin_family      = AF_INET; // ipv4 socket
 	server_addr.sin_port        = htons((u_short)atoi(argv[1]));
 
 	// struct for client conection
@@ -94,6 +93,11 @@ int main(int argc, char **argv){
 
 	printf("waiting for incoming connections...\n");
 	while (1) {
+
+		if (CVector_length(started_threads) >= MAX_THREADS){
+			usleep(200);
+			continue;
+		}
 		
 		// wait for incoming TCP connection (connect() call from somewhere else)
 		if ((client_sockfd = accept(server_sockfd, (struct sockaddr *) &client_addr, &addrlen)) < 0){ // blocking call
@@ -101,18 +105,22 @@ int main(int argc, char **argv){
 		}
 
 		// prepare args and create separate thread to handle connection
-		const thread_args_t th_args = {client_sockfd, started_threads_counter, client_addr, addrlen};
-		pthread_create(&started_threads[started_threads_counter++], NULL, (void *) &connection_thread, (void *) &th_args);	
-		// TODO adapt thread array or how to place them in	
+		const thread_args_t th_args = {client_sockfd, client_id_counter, client_addr, addrlen};
+		pthread_create(&tidBUF, NULL, (void *) &connection_thread, (void *) &th_args);
+		
+		// append tid to started_threads vector
+		CVector_append(started_threads, &tidBUF);
+		client_id_counter++;
 	}
 
 	try_close(server_sockfd);
+	CVector_free(started_threads);
 	return EXIT_SUCCESS;
 }
 
 static void connection_thread(void * th_args) {
 
-	// unsure: values in th_args could get overwritten in main thread, only the pointer is const
+	// unsure: values in th_args could get overwritten in main thread
 	// make local copy of args
 	thread_args_t* args = (thread_args_t*)malloc(sizeof(thread_args_t));
 	memcpy(args, (thread_args_t*) th_args, sizeof(thread_args_t));
@@ -124,12 +132,13 @@ static void connection_thread(void * th_args) {
 	memset(recvBUF, 0, BUFSIZE);
 
 	// setup exit-handler
-	pthread_cleanup_push((void *) &thread_exithdlr, (void *) args);
+	pthread_cleanup_push((void *) &thread_exithandler, (void *) args);
 
 	printf("Connection accepted from: %s (client %d)\n", inet_ntoa(args->client_addr.sin_addr), args->clientnr);
 
 	// as long as data arrives, print data
-	while ((msg_len = recvfrom(args->connfd, &recvBUF, BUFSIZE-1, 0, (struct sockaddr *) &args->client_addr, &args->addrlen)) >= 0) {
+	while (!exit_requested && 
+			(msg_len = recvfrom(args->connfd, &recvBUF, BUFSIZE-1, 0, (struct sockaddr *) &args->client_addr, &args->addrlen)) >= 0) {
 		
 		if (msg_len == 0){
 			printf("Closing connection to client %d (%s)\n", args->clientnr, inet_ntoa(args->client_addr.sin_addr));
@@ -154,8 +163,34 @@ static void connection_thread(void * th_args) {
 	pthread_exit((void *)pthread_self());
 }
 
-static void thread_exithdlr(void * th_args) {
+static void thread_exithandler(void * th_args) {
+	// close socket on exit
 	thread_args_t *args = (thread_args_t *) th_args;
 	printf("client %d: ", args->clientnr);
 	try_close(args->connfd);
+}
+
+// ensure closing socket when recieving SIGINT
+static void sighandler(int signo){
+	// pthread_exit should invoke the exit-handler of the threads
+	// TODO only use signal-safe functions
+
+	switch (signo){
+		case SIGINT:
+			printf("\nSIGINT recieved. Closing sockets\n");
+			close(server_sockfd);
+			for (int i=0; i < CVector_length(started_threads); i++){
+				pthread_exit(CVector_get(started_threads, i));
+				printf("thread closed\n");
+			}
+			printf("exiting\n");
+			exit(EXIT_SUCCESS);
+			break;
+		case SIGTERM:
+			printf("\nSIGTERM recieved. Closing server socket: ");
+			try_close(server_sockfd);
+			exit(EXIT_FAILURE);
+		default:
+			break;
+	}
 }
